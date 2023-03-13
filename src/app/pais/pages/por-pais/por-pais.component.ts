641d8202f4305e41330aa0ab3b702163e0f46f32
María José Medina <mjmedina.pj@gmail.com>
2023-03-13 12:59:30 -0300

    Feat: Servicio para buscar paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
     selector: 'app-por-pais',
@@ -10,7 +11,14 @@ export class PorPaisComponent {
 
     termino: string = '';
 
+    constructor(private paisService: PaisService) {}
+
     buscar() {
         console.log(this.termino);
+
+        this.paisService.buscarPais(this.termino)
+            .subscribe(resp => {
+                console.log(resp);
+            });
     }
 }
