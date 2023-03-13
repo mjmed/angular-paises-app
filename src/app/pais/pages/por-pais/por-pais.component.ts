ca033a399b575b63ce451507d72bd139ae59c100
María José Medina <mjmedina.pj@gmail.com>
2023-03-13 12:45:30 -0300

    Feat: Componente para buscar por pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+    selector: 'app-por-pais',
+    templateUrl: './por-pais.component.html',
+    styles: [
+    ]
 })
 export class PorPaisComponent {
 
+    termino: string = '';
+
+    buscar() {
+        console.log(this.termino);
+    }
 }
