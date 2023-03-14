7b5c2f3208d3732d29d5061aabbace6b78ce5617
María José Medina <mjmedina.pj@gmail.com>
2023-03-14 11:30:16 -0300

    Feat: Ver pais de forma independiente

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
-  selector: 'app-ver-pais',
-  templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+    selector: 'app-ver-pais',
+    templateUrl: './ver-pais.component.html',
+    styles: [
+    ]
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit {
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private paisService: PaisService
+    ) {}
+
+    ngOnInit(): void {
+        this.activatedRoute.params
+            .subscribe(({ id }) => {
+                this.paisService.getPaisPorAlpha(id)
+                    .subscribe(pais => {
+                        console.log('pais:', pais)
+                    })
+            });
+    }
 
 }
