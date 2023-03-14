ba307a948b0b9d5584f9895d6369e05327b3746e
María José Medina <mjmedina.pj@gmail.com>
2023-03-14 09:16:18 -0300

    Feat: Componente Input y Tabla

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-pais-input',
+    templateUrl: './pais-input.component.html',
+    styles: [
+    ]
+})
+export class PaisInputComponent {
+
+    termino: string = '';
+
+    buscar() {
+        console.log(this.termino);
+    }
+}
